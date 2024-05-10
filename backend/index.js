ddaf2a9622c443f9eeb8d15bd2681dcb3e5ff051
TibePrincenPXL <tibe.princen@student.pxl.be>
2024-05-10 16:30:39 +0200

    test git ignore

--- backend/index.js
+++ backend/index.js
@@ -8,11 +8,11 @@ const cors = require('cors')
 const port=3000;
 
 const pool = new pg.Pool({
-    user: 'secadv',
-    host: 'db',
-    database: 'pxldb',
-    password: 'ilovesecurity',
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
     connectionTimeoutMillis: 5000
 })
 
